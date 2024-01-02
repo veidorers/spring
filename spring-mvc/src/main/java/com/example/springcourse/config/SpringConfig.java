66a605c978880b80a4b6abf097d2a270df698d4f
Sergey Fomenko <artem.work749@gmail.com>
2024-01-02 11:37:27 +0300

    Lesson 3 - Configuration via Java code

---
+++ spring-mvc/src/main/java/com/example/springcourse/config/SpringConfig.java
@@ -0,0 +1,49 @@
+package com.example.springcourse.config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+@Configuration
+@ComponentScan("com.example.springcourse")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+    private ApplicationContext applicationContext;
+
+    public SpringConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver getTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
+        resolver.setPrefix("WEB-INF/views/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(getTemplateResolver());
+        engine.setEnableSpringELCompiler(true);
+        return engine;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(resolver);
+    }
+}
+
+
