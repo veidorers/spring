ef8727a5b4fc19b1b0b6351bab43df138e04259e
Sergey Fomenko <artem.work749@gmail.com>
2024-01-02 14:54:21 +0300

    Lesson 8 - CRUD, REST, DAO

---
+++ spring-mvc/src/main/java/com/example/springcourse/config/dao/PersonDAO.java
@@ -0,0 +1,29 @@
+package com.example.springcourse.config.dao;
+
+import com.example.springcourse.config.model.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+    private static int PEOPLE_COUNT = 0;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Alex"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Michael"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
