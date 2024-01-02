d9e6f27221b4bcb2acca1ecf337f04655fad7125
Sergey Fomenko <artem.work749@gmail.com>
2024-01-02 15:54:52 +0300

    Lesson 9 - ModelAttribute annotation. HTML forms (and HTML forms with Thymeleaf)

--- spring-mvc/src/main/java/com/example/springcourse/config/controller/PeopleController.java
+++ spring-mvc/src/main/java/com/example/springcourse/config/controller/PeopleController.java
@@ -1,12 +1,11 @@
 package com.example.springcourse.config.controller;
 
 import com.example.springcourse.config.dao.PersonDAO;
+import com.example.springcourse.config.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("people")
@@ -29,5 +28,21 @@ public class PeopleController {
                        Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @ModelAttribute("greeting")
+    public String  getGreeting() {
+        return "Welcome to our site!";
     }
 }
