d12c8aeb71df1f63cee00b4ff26c729b8dee4f81
Sergey Fomenko <artem.work749@gmail.com>
2024-01-02 11:59:33 +0300

    Lesson 4 - Controllers and annotation Controller

---
+++ spring-mvc/src/main/java/com/example/springcourse/controller/FirstController.java
@@ -0,0 +1,19 @@
+package com.example.springcourse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String sayGoodBye() {
+        return "first/goodbye";
+    }
+}
